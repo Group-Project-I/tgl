f4c4c482d7a8dc34c3c5da1ec236727b7450bc70
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-02-16 15:16:09 +0530

    Completed hires pdf and print preview views

--- src/components/admin/dashboard/vehicles/ManageVehicle.js
+++ src/components/admin/dashboard/vehicles/ManageVehicle.js
@@ -61,10 +61,10 @@ class ManageVehicle extends Component {
                                 <Tab>Exports</Tab>
                             </TabList>
                             <TabPanel>
-                                <Imports importHires={importHires}></Imports>
+                                <Imports importHires={importHires} history={this.props.history}></Imports>
                             </TabPanel>
                             <TabPanel>
-                                <Exports exportHires={exportHires}></Exports>
+                                <Exports exportHires={exportHires} history={this.props.history}></Exports>
                             </TabPanel>
                         </Tabs>
                     </TabPanel>
